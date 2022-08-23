892895e20ea83e1f3a0b08e788c6fbfd8851b661
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-08-23 12:07:23 -0400

    function about getting review by index

--- index.js
+++ index.js
@@ -124,11 +124,11 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, index) {
+  return `${array[index].name} gave the restaurant a ${array[index].rating} star review, and their feedback was: ${array[index].feedback}`;
 }
 
-  
+console.log("Task 6: ", getReviewByIndex(reviews, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Write a function to get information about the most recent (last) review called `getLastReview`
